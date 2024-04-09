bd5b3f830982c156fedaacf210e75657a7bb941e
Vitor Vieira <vitor.vieira@southsystem.com.br>
2024-04-09 13:08:09 -0300

    Adiciona dependencias e estrutura do projeto - Atualiza versão Java 1.8 -> 17

--- app/src/main/java/com/example/shopmie/ui/MainActivity.kt
+++ app/src/main/java/com/example/shopmie/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shopmie
+package com.example.shopmie.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shopmie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
